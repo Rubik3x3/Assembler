7a05eab2093606cfbebcacbe1df4c60b15c58658
Rubik3x3 <franco.f4629@gmail.com>
2024-06-11 16:23:59 -0300

    3

---
+++ ejercicios/3.asm
@@ -0,0 +1,8 @@
+MOV AL, 4 ; Mover a AL el 4
+MOV BL, 10 ; Mover a BL el 16 (decimal)
+
+ADD AL, BL ; Sumar a AL, lo que esta en BL
+
+MOV [50], AL ; Mover a al posición 50 de la RAM lo que esta en AL
+
+END
