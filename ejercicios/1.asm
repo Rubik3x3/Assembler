2e7ef8434e89f5b76e01ccd27731f7cb017ee7cf
Rubik3x3 <franco.f4629@gmail.com>
2024-06-11 16:19:29 -0300

    2

--- ejercicios/1.asm
+++ ejercicios/1.asm
@@ -1,4 +1,4 @@
 ; Mover al registro AL el 4 y al registro BL el 16
 MOV AL, 4
-MOV BL, 16
+MOV BL, 10
 END
---
+++ ejercicios/2.asm
@@ -0,0 +1,7 @@
+; Mover al registro AL el 4, al registro BL el 16 y sumar a AL, BL
+MOV AL, 4
+MOV BL, 10
+
+ADD AL, BL
+
+END
